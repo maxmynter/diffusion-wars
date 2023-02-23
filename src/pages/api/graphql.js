99cb658f22553e893681112aa1d5c633080c789d
Max Mynter <maxmynter@me.com>
2023-02-23 23:25:37 +0100

    GraphQL works with Playground

--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -1,9 +1,14 @@
 import { ApolloServer } from "apollo-server-micro";
-import Cors from "micro-cors";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import typeDefs from "./schemas/uploadImage";
 import resolvers from "./resolvers/uploadImage";
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
 
 export const config = {
   api: {
@@ -11,11 +16,9 @@ export const config = {
   },
 };
 
-const cors = Cors();
-
 const startServer = server.start();
 
-export default cors(async (req, res) => {
+export default async function handler(req, res) {
   await startServer;
   await server.createHandler({ path: "/api/graphql" })(req, res);
-});
+}
